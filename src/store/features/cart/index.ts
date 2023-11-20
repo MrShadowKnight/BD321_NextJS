f8c77c752397b25a4483ef2522dddbf83fbb347e
MrShadowKnight <volodymyr.pinchak@gmail.com>
2023-11-20 18:44:51 +0200

    Add.changes

--- src/store/features/cart/index.ts
+++ src/store/features/cart/index.ts
@@ -1,11 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export interface cartState {
-  items: any[];
+export interface CartState {
+  items: number[]; // Assuming you're storing numbers in the cart
 }
 
-const initialState: cartState = {
+const initialState: CartState = {
   items: [],
 };
 
@@ -18,25 +17,14 @@ export const cartSlice = createSlice({
       // set to LS
       localStorage.setItem("cart", JSON.stringify(state.items));
     },
-    removeItemFromCart: (state, action: PayloadAction<number>) => {
-      const productID = action.payload;
-      // check if exist product ID in state
-      const isExist = state.items.indexOf(productID);
-      // cond
-      if (isExist != -1) {
-        state.items = state.items.filter((item) => item != productID);
-      } else {
-        alert("Product doesn't exist in cart!");
-      }
-    },
-    addDataFromLS: (state, action: PayloadAction<[]>) => {
+    addDataFromLS: (state, action: PayloadAction<number[]>) => {
       state.items = [...action.payload];
     },
   },
 });
 
 // export reducer
-export const { addItemToCart, addDataFromLS, removeItemFromCart } =
-  cartSlice.actions;
-// export slice
 export default cartSlice.reducer;
+
+// export actions
+export const { addItemToCart, addDataFromLS } = cartSlice.actions;

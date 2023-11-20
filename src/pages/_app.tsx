294ef9c89511cdd3d35c7c5c4ce226c9823c6a85
MrShadowKnight <volodymyr.pinchak@gmail.com>
2023-11-20 17:05:54 +0200

    Add.changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,7 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { useEffect } from "react";
-// layout
-import Layout from "@/layouts";
-// redux
-import { Provider } from "react-redux";
-import { store } from "../store";
-import { useDispatch } from "react-redux";
-// import { addDataFromLS } from "@/store/features/cart";
+import '@/styles/globals.css'
+import Link from 'next/link'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  // setting redux
-  useEffect(() => {
-    console.log("test_____");
-  });
-  return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
-  );
+  return <Component {...pageProps} />
 }
